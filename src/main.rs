4946bdeaa2bd5b5e2fef2d8cc4c5498e59b477da
IbukiShinzato <e235718@ie.u-ryukyu.ac.jp>
2024-11-25 01:34:29 +0900

    view list

--- src/main.rs
+++ src/main.rs
@@ -17,55 +17,115 @@ struct Task {
 fn main() -> io::Result<()> {
     println!("Todo List!");
 
-    // let mut entries = fs::read_dir("list")?
-    //     .map(|res| res.map(|e| e.path()))
-    //     .collect::<Result<Vec<_>, io::Error>>()?;
+    // サンプル
+    let mut lists: Vec<List> = Vec::new();
 
-    // entries.sort();
+    let tasks: Vec<Task> = Vec::new();
+    let mut list: List = List {
+        name: String::from("OS"),
+        tasks: tasks,
+    };
 
-    // let mut lists: Vec<List> = Vec::new();
+    let task = Task {
+        name: String::from("OS3.3"),
+        done: true,
+    };
 
-    // let tasks: Vec<Task> = Vec::new();
-    // let mut list: List = List {
-    //     name: String::from("OS"),
-    //     tasks: tasks,
-    // };
+    list.tasks.push(task);
+    lists.push(list);
 
-    // let task = Task {
-    //     name: String::from("OS1.3"),
-    //     done: true,
-    // };
+    println!("{:?}", lists);
 
-    // list.tasks.push(task);
-
-    // lists.push(list);
-
-    // println!("{:?}", lists);
+    let mut home_flag = true;
 
     loop {
-        // 入力待ち状態
-        println!(
-            "\nEnter a command: 
-    stop => exit program
-    s list => show list
-    s task => show task in list
-    c list => create list
-    c task => create task in list"
-        );
+        // リストの操作
+        if home_flag {
+            // 入力待ち状態
+            println!(
+                "\nEnter a command: 
+    show => show lists
+    create => create list
+    change => change task mode
+    exit => exit program\n"
+            );
 
-        // 入力受け取り
-        let mut input = String::new();
-        io::stdin()
-            .read_line(&mut input)
-            .expect("failed to readline");
+            // 入力受け取り
+            let mut input = String::new();
+            io::stdin()
+                .read_line(&mut input)
+                .expect("failed to readline");
 
-        // 入力の改行などを取り除く
-        let command = input.trim();
+            // 入力の改行などを取り除く
+            let command = input.trim();
 
-        // 入力の内容に応じた処理
-        match command {
-            "stop" => break,
-            _ => continue,
+            // 入力の内容に応じた処理
+            match command {
+                "show" => {
+                    println!();
+                    let mut num: usize = 1;
+                    for list in &lists {
+                        println!("{}: {}", num, list.name);
+                        num += 1;
+                    }
+                }
+                "create" => {}
+                "change" => {
+                    home_flag = false;
+                    // 入力待ち状態
+                    println!("which list would you choose?");
+
+                    // 入力受け取り
+                    let mut input = String::new();
+                    io::stdin()
+                        .read_line(&mut input)
+                        .expect("failed to readline");
+
+                    //　入力の改行などを取り除く
+                    let index = input.trim().parse::<usize>().unwrap();
+                }
+                "exit" => break,
+                "" => continue,
+                _ => println!("command not found"),
+            }
+
+        // タスクの操作
+        } else {
+            // 入力待ち状態
+            println!(
+                "\nEnter a command: 
+    show => show tasks
+    create => create task
+    change => change list mode
+    exit => exit program\n"
+            );
+
+            // 入力受け取り
+            let mut input = String::new();
+            io::stdin()
+                .read_line(&mut input)
+                .expect("failed to readline");
+
+            // 入力の改行などを取り除く
+            let command = input.trim();
+
+            // 入力の内容に応じた処理
+            match command {
+                "show" => {
+                    // // リストの取得
+                    // let mut list = lists.get(index - 1).unwrap();
+
+                    // let mut num: usize = 1;
+                    // for task in &list.tasks {
+                    //     println!("{}: {:?}", num, task);
+                    //     num += 1;
+                    // }
+                }
+                "create" => {}
+                "change" => home_flag = true,
+                "" => continue,
+                _ => println!("command not found"),
+            }
         }
     }
 
